1c681c75d32ec052dfeebe5d736413ddaa3fdc09
techpanda411 <techpanda411@gmail.com>
2024-11-21 13:57:34 -0500

    fix collapsible functionality

--- styles/collapsible.js
+++ styles/collapsible.js
@@ -2,6 +2,7 @@ var coll_elems = document.getElementsByClassName("collapsible");
 
 for (var i = 0; i < coll_elems.length; i++) {
     coll_elems[i].addEventListener("click", function () {
+        this.classList.toggle("active");
         var content = this.nextElementSibling;
         if (content.style.display === "block") {
             content.style.display = "none";
