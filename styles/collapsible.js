379c59e4022043854208134d89c7f82a25bf8706
techpanda411 <techpanda411@gmail.com>
2024-11-21 13:26:56 -0500

    added collapsible functionality to section headers

---
+++ styles/collapsible.js
@@ -0,0 +1,12 @@
+var coll_elems = document.getElementsByClassName("collapsible");
+
+for (var i = 0; i < coll_elems.length; i++) {
+    coll_elems[i].addEventListener("click", function () {
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
