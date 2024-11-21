9c08fa80f6daa3236c8329d0895a88ea951d5fa3
techpanda411 <techpanda411@gmail.com>
2024-11-21 14:32:44 -0500

    wait until page loads before executing javascript for collapsible functionality

--- scripts/collapsible.js
+++ scripts/collapsible.js
@@ -1,13 +1,15 @@
-var coll_elems = document.getElementsByClassName("collapsible");
+window.addEventListener('load', (event) => {
+    var coll_elems = document.getElementsByClassName("collapsible");
 
-for (var i = 0; i < coll_elems.length; i++) {
-    coll_elems[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.display === "block") {
-            content.style.display = "none";
-        } else {
-            content.style.display = "block";
-        }
-    });
-}
+    for (var i = 0; i < coll_elems.length; i++) {
+        coll_elems[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
+});
