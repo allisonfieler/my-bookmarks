ad7f5a7db540b344ec186f7a8100418c95981afd
techpanda411 <techpanda411@gmail.com>
2024-11-22 08:23:10 -0500

    added plus/minus icons to collapsible sections

--- scripts/collapsible.js
+++ scripts/collapsible.js
@@ -3,6 +3,7 @@ window.addEventListener('load', (event) => {
 
     for (var i = 0; i < coll_elems.length; i++) {
         coll_elems[i].addEventListener("click", function () {
+            this.classList.toggle("active");
             var content = this.nextElementSibling;
             if (content.style.display === "none") {
                 content.style.display = "block";
